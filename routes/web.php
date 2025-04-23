de35f271d626dee64283f8325dcb5b3cd3c68f03
AthelstanMCCV <165026195+AthelstanMCCV@users.noreply.github.com>
2025-04-23 20:02:16 +0800

    Login & Signup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('homepage');
 });
+
+Route::get('/dashboard', function () {
+    return view('admin-dashboard');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/MyWmsu', function () {
+    return view('sign-in');
+});
+
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
